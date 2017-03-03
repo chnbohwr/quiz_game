ed08b26cbf610a3ae5afb89c157c016c57e73840
Hyman_Chen <hyman_chen@wistron.com>
2017-03-03 11:45:56 +0800

    fix port for heroku

--- server.js
+++ server.js
@@ -25,16 +25,7 @@ var SampleApp = function () {
      *  Set up server IP address and port # using env variables/defaults.
      */
     self.setupVariables = function () {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
+        self.port = process.env.PORT || 8080;
     };
 
 
@@ -101,9 +92,8 @@ var SampleApp = function () {
      */
     self.start = function () {
         //  Start the app on the specific interface (and port).
-        server.listen(self.port, self.ipaddress, function () {
-            console.log('%s: Node server started on %s:%d ...',
-                Date(Date.now()), self.ipaddress, self.port);
+        server.listen(self.port, function () {
+            console.log(`Server start on port ${self.port}`);
         });
     };
 
