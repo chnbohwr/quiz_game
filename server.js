d5e82e534e17f80adbf3d9413f3878a0916f5e93
Ric_Chen <Ric_Chen@wistron.com>
2017-03-03 14:48:21 +0800

    refactor server.js

--- server.js
+++ server.js
@@ -1,109 +1,29 @@
 #!/bin/env node
- //  OpenShift sample Node application
-var express = require('express');
-var fs = require('fs');
-var app = require('express')();
-var server = require('http').Server(app);
-//open shift only allow websocket in port 8000
-var io = require('socket.io')(server);
-var gameSocket = require('./server_src/socket.js');
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function () {
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
-    //  Scope.
-    var self = this;
+// require game main file.
+const gameSocket = require('./server_src/socket.js');
 
+// get port from env setting
+const port = process.env.PORT || 8080;
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+// create express app
+const app = express();
+const server = http.Server(app);
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function () {
-        self.port = process.env.PORT || 8080;
-    };
+// create socket io server
+const io = socketIO(server);
 
+// set static folder
+app.use('/', express.static(path.join(__dirname, 'web_src')));
 
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function (sig) {
-        if (typeof sig === "string") {
-            console.log('%s: Received %s - terminating sample app ...',
-                Date(Date.now()), sig);
-            process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()));
-    };
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function () {
-        //  Process on exit and signals.
-        process.on('exit', function () {
-            self.terminator();
-        });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function (element, index, array) {
-            process.on(element, function () {
-                self.terminator(element);
-            });
-        });
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function () {
-        app.use('/', express.static(__dirname + '/web_src/'));
-    };
-
-    self.initializeSocket = function () {
-        gameSocket(io);
-    };
-    
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function () {
-        self.setupVariables();
-        self.setupTerminationHandlers();
-        // Create the express server and routes.
-        self.initializeServer();
-        self.initializeSocket();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function () {
-        //  Start the app on the specific interface (and port).
-        server.listen(self.port, function () {
-            console.log(`Server start on port ${self.port}`);
-        });
-    };
-
-}; /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
+// run http server
+server.listen(port, () => {
+  // start game socket server
+  gameSocket(io);
+  console.log(`Server start on port ${port}`);
+});
